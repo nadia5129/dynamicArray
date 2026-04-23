ba02959e92a5b659ce1feffce029059a8bd13a6f
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-04-23 12:14:39 -0700

    added the starting implementation of all required methods to get rid of error

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,21 +1,36 @@
 public class DynamicStringList implements StringList{
 
-    public String [] arr;
-    public int size;
+    private String[] arr;
+    private int size;
 
     public DynamicStringList(){
         arr = new String[4];
-        size = 0;
+        this.size = 0;
     }
     public String get(int index){
         if (index < 0 || index >= size){
             throw new IndexOutOfBoundsException();
         } 
         return arr[index];
-        }
     }
 
-    
-    
-    
+    public void set(int index, String value){
+
+    }
+
+    public void add(String value){
+
+    }
+
+    public String remove(int index){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public int capacity(){
+        return 0;
+    }
 }
