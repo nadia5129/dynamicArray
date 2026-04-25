0697e2aec6e3dde5d85ebff18cfb733549733300
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-04-24 21:43:01 -0700

    added test for methods add/get

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+
 public class DynamicStringListTest {
-    
+    @Test
+    public void testAddAndGet() {
+        StringList list = new DynamicStringList();
+        list.add("Hello");
+        list.add("World");
+        assert list.get(0).equals("Hello");
+        assert list.get(1).equals("World");
+    }
 }
